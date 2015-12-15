9671022a3dc218c169b959e0699385cfd9e64378
Pradeep Talwar <ptal528@yahoo.com>
2015-12-15 16:54:58 -0500

    Added remote DB

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	'port': process.env.PORT || 3000,
-	'database':"mongodb://localhost:27017/booktrading",
+	'database':"mongodb://ptalwar15:Coolit12@ds047722.mongolab.com:47722/booktrading",
 	'secret':'ilovescotchscotchyscotchscotch'
 }
+
+//local DB: mongodb://localhost:27017/booktrading //
